78828f690852a215b9ba20abe8355fc18fc0293d
grapswiz <ineeza@gmail.com>
2013-08-19 16:03:06 +0900

    HTMLで指定した画像をページの背景にするdirectiveを作った

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ angular-parts
 
 angularjsを使った画面とかdirectiveとか
 
-[shortcut-key](http://grapswiz.com/angular-parts/shortcut-key/): jとkで上下に移動するニュースリーダー風なUIをdirectiveで作った。
+[shortcut-key](http://grapswiz.com/angular-parts/shortcut-key/): jとkで上下に移動するニュースリーダー風なUIをdirectiveで作った。  
+[background-image](http://grapswiz.com/angular-parts/background-image/): HTMLで指定した画像をページの背景にするdirective
---
+++ background-image/index.js
@@ -0,0 +1,27 @@
+BACKGROUNDS = ['tree.jpg', 'hydrangea.jpg', 'flower.jpg'];
+
+var app = angular.module('app', []);
+
+app.controller('ResourceManageController', ['$scope', function ($scope) {
+    $scope.background = BACKGROUNDS[Math.floor(Math.random() * BACKGROUNDS.length)];
+}]);
+
+app.directive('pageBackground', function($parse) {
+    return {
+        link: function (scope, elm, attrs) {
+            scope.$watch(attrs.pageBackground, function (url) {
+                if (angular.isUndefined(url)) {
+                    return;
+                }
+                elm.css({
+                    'background': 'url(' + url + ') no-repeat center center fixed',
+                    '-webkit-background-size': 'cover',
+                    '-moz-background-size': 'cover',
+                    '-o-background-size': 'cover',
+                    'background-size': 'cover'
+                });
+            });
+        }
+    };
+
+});
